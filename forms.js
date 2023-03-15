3be71f28718d902ab6fad6f13cc92a470f02ccbf
Jesus Coto <jesus@thebranx.com>
2023-03-15 13:46:31 +0100

    Update forms.js

--- forms.js
+++ forms.js
@@ -10,19 +10,46 @@ $(document).on('submit', 'form', function() {
 });
 
 // Dynamic params to inputs
-var getUrlParameter = function (sParam) {
+var getUrlParameter = function(sParam) {
     var params = new URLSearchParams(window.location.search);
     return params.has(sParam) ? decodeURIComponent(params.get(sParam)) : false;
   };
   
-  $(document).ready(function () {
+  $(document).ready(function() {
     var params = Array.from(new URLSearchParams(window.location.search));
     for (var [key, value] of params) {
-      var input = $('input[name="' + key + '"]');
-      if (input.length && value) {
-        input.val(value);
-      }
+      var element = $('[name="' + key + '"]');
+        element.val(value);
     }
   });
 
-  
+
+//   Auto select Service from URL param
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+    $( document ).ready(function() {
+        var type = getUrlParameter('type');
+        $('input[value="'+ type +'"]').click();
+        
+        var sector = getUrlParameter('sector');
+        if (sector.length > 0) {
+       		 $("#sector").val(sector);    
+				};
+    });
+
+// Selects default texts of dropdown input
+$( "select option:nth-child(1)" ).attr("disabled","disabled");
