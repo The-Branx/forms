2a715bf7d9426a7c1263ec533e4c81dc9cddc3c3
Jesus Coto <jesus@thebranx.com>
2023-03-15 18:28:57 +0100

    Fixed everything

--- forms.js
+++ forms.js
@@ -1,32 +1,38 @@
-// Scrolls to top and indetifies user on Submit
-$(document).on('submit', 'form', function() {
-	setTimeout(function(){
+// Scrolls user to success message
+$(document).on('submit', 'form', function () {
+    setTimeout(function () {
         $('html, body').animate({
-            scrollTop: 0
-        }, 400);
+            scrollTop: $('.w-form-done').offset().top
+        }, 400); //
     }, 500);
-  let email_address = $("input[name='email']").val();
-  sendinblue.identify(email_address);
 });
 
 // Dynamic params to inputs
-
-var getUrlParameter = function(sParam) {
+var getUrlParameter = function (sParam) {
     var params = new URLSearchParams(window.location.search);
     return params.has(sParam) ? decodeURIComponent(params.get(sParam)) : false;
-  };
-  $(document).ready(function() {
-  	//exception for services
+};
+$(document).ready(function () {
+    //exception for services
     var type = getUrlParameter('type');
-    $('input[value="'+ type +'"]').click();
-		//rest
+    $('input[value="' + type + '"]').click();
+    //rest
     var params = Array.from(new URLSearchParams(window.location.search));
     for (var [key, value] of params) {
         var element = $('[name="' + key + '"]:not([type="checkbox"])');
         element.val(value);
     }
-  });
-
+    // stores language
+    var langUrl = window.location.hostname.split('.')[0];
+    if (!langUrl || langUrl === 'www' || langUrl === 'thebranx') {
+        langUrl = 'en';
+    } else {
+        langUrl = langUrl.toLowerCase();
+    }
+    $('input[name="lang-url"], #lang-url').val(langUrl);
+    // honeypot
+    $("#Privacy-Check").hide();
+});
 
 // Selects default texts of dropdown input
-$( "select option:nth-child(1)" ).attr("disabled","disabled");
+$("select option:nth-child(1)").attr("disabled", "disabled");
