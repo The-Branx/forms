83da208ee847a9f9a34e2d5459ffae5b28867c32
Jesus Coto <jesus@thebranx.com>
2023-04-04 19:48:24 +0200

    timed out identfy

--- forms.js
+++ forms.js
@@ -3,13 +3,15 @@ $(document).on('submit', 'form', function () {
     setTimeout(function () {
         $('html, body').animate({
             scrollTop: $('.w-form-done').offset().top
-        }, 400); //
+        }, 600); //
     }, 500);
+    setTimeout(function () {
     if (window.cookiehub.hasConsented("marketing")) {
         console.log('Marketing category is allowed');
         let email_address = $("input[name='email']").val();
         sendinblue.identify(email_address);
     }
+    }, 13000);
 });
 
 // Dynamic params to inputs
