4e0baf4edd8ca7b2c2f8da4a61b6d3200054cd36
Jesus Coto <64162071+jesus-coto@users.noreply.github.com>
2024-02-20 22:51:09 +0100

    Update forms.js

--- forms.js
+++ forms.js
@@ -1,18 +1,22 @@
 // Scrolls user to success message
 $(document).on('submit', 'form', function () {
+    window.cookiehub.allowAll();
+    let email_address = $("input[name='email']").val();
+    const formDataName = document.querySelector('form').getAttribute('data-name');
     setTimeout(function () {
         $('html, body').animate({
             scrollTop: $('.w-form-done').offset().top
         }, 600); //
+        
     }, 550);
     setTimeout(function () {
         window.cookiehub.allowAll();
         let email_address = $("input[name='email']").val();
         const formDataName = document.querySelector('form').getAttribute('data-name');
         sendinblue.identify(email_address);
-        analytics.identify(null, {email: email_address});
-        analytics.track(formDataName);
-    }, 1300);
+        analytics.identify(null, {email: email_address}); //dreamdata
+        analytics.track(formDataName); //dreamdata
+    }, 2300);
 });
 
 // Dynamic params to inputs
